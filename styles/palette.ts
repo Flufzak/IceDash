4f42b6f434542fcb199d3d9095b58a3a82fecfc6
Flufzak <khachatryan.suzi@gmail.com>
2026-01-01 19:38:20 +0100

    palette changes

--- styles/palette.ts
+++ styles/palette.ts
@@ -1,6 +1,13 @@
 export const palette = {
   primary: "#a4e2fd",
-  /* oude kluer 45e6f6 */
+  /* oude kleur 45e6f6 */
+
+  /* dialog - buttons */
+  pink: "#fc5db4",
+  lightpink: "#fa80cb",
+  darkblue: "#0089e7",
+
+  /* algemeen */
   ground: "#bfe9ff",
   background: "#68a2ddff",
   shadow: "#57749aff",
